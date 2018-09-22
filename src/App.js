cfd1ccac8e03ff09641ae6615200762d023b487f
Sean Huang <hyprul@ucla.edu>
2018-09-22 18:15:10 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { Component } from 'react';
+import Greeting from './components/Greeting.js';
+import Button from './components/Button.js';
+
+
+export default class App extends Component {
+
+	state = {
+		peopleInSpace: []
+	}
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json')
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					peopleInSpace: data.people
+				})
+			})
+	};
+
+	render() {
+
+		return (
+			<div>
+				< Greeting />
+				< Button />
+			</div>
+		)
+	}
+}
